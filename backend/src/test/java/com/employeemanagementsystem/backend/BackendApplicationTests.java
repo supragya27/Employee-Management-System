22f43320243d7de622a4c9cd5fabebc0873beca7
supragya27 <supragya.chauhan001@gmail.com>
2020-12-29 21:11:29 +0530

    initial commit

---
+++ backend/src/test/java/com/employeemanagementsystem/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeemanagementsystem.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
