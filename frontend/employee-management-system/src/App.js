9a91a3ea17c6a4b26137e14e2dfffddacc89395c
supragya27 <supragya.chauhan001@gmail.com>
2020-12-31 01:43:21 +0530

    routing update

--- frontend/employee-management-system/src/App.js
+++ frontend/employee-management-system/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ListEmployee from './components/ListEmployee'
 import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import './App.css'
 
 function App() {
   return (
-    <>
-    <Header/>
-    <Container>
-      <ListEmployee/>
-    </Container>
-    <Footer/>
-    </>
+    <div>
+      <Router>
+        <>
+          <Header/>
+            <Container>
+              <Switch>               
+                <Route path="/" component={ListEmployee}></Route>
+                <Route path="/employees" component={ListEmployee}></Route>
+              </Switch>
+            </Container>
+          <Footer/>
+        </>
+      </Router>
+    </div>
     )
 }
 
